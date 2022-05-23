2bac7bc8e06cea9a7cf1bf38ee66662d9ff86d14
Nicolas Ramirez <ramirez.nicolas2305@hotmail.com>
2022-05-23 15:26:53 +0200

    adding music

--- Found_it/src/application/Main.java
+++ Found_it/src/application/Main.java
@@ -1,29 +1,43 @@
 package application;
-
+ 
+import java.nio.file.Paths;
+ 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-
+ 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = FXMLLoader.load(getClass().getResource("vue/vue1.fxml"));
-			Scene scene = new Scene(root,960,544);
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
-			primaryStage.setFullScreen(true);
-			primaryStage.show();
-			root.requestFocus();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+ 
+private MediaPlayer mediaPlayer;
+ 
+@Override
+public void start(Stage primaryStage) {
+try {
+music();
+BorderPane root = FXMLLoader.load(getClass().getResource("vue/vue1.fxml"));
+Scene scene = new Scene(root,960,544);
+primaryStage.setScene(scene);
+primaryStage.setResizable(false);
+primaryStage.setFullScreen(true);
+primaryStage.show();
+root.requestFocus();
+} catch(Exception e) {
+e.printStackTrace();
 }
-
+}
+ 
+public void music() {
+String s = "/home/etudiants/info/nramirez/prive/Found_it/Found_it/src/application/music.mp3";
+Media media = new Media(Paths.get(s).toUri().toString());
+mediaPlayer = new MediaPlayer(media);
+mediaPlayer.play();
+}
+ 
+public static void main(String[] args) {
+launch(args);
+}
+}
